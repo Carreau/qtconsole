caaf08e61cef2055505f50dd3a3be8e382c2228b
M Bussonnier <bussonniermatthias@gmail.com>
2024-10-28 11:17:09 +0100

    try async

--- qtconsole/tests/test_inprocess_kernel.py
+++ qtconsole/tests/test_inprocess_kernel.py
@@ -4,28 +4,35 @@
 # Distributed under the terms of the Modified BSD License.
 
 import unittest
-
+import inspect
+import asyncio
 from qtconsole.inprocess import QtInProcessKernelManager
+import ipykernel
 
 
-class InProcessTests(unittest.TestCase):
+class InProcessTests(unittest.IsolatedAsyncioTestCase):
 
-    def setUp(self):
+    async def asyncSetUp(self):
         """Open an in-process kernel."""
         self.kernel_manager = QtInProcessKernelManager()
-        self.kernel_manager.start_kernel()
+        if ipykernel.version_info >= (7,):
+            self.task = asyncio.create_task(self.kernel_manager.start_kernel())
+        else:
+            self.kernel_manager.start_kernel()
         self.kernel_client = self.kernel_manager.client()
 
-    def tearDown(self):
+    async def asyncTearDown(self):
         """Shutdown the in-process kernel. """
         self.kernel_client.stop_channels()
         self.kernel_manager.shutdown_kernel()
+        await self.task
 
-    def test_execute(self):
+    async def test_execute(self):
         """Test execution of shell commands."""
         # check that closed works as expected
         assert not self.kernel_client.iopub_channel.closed()
         
         # check that running code works
-        self.kernel_client.execute('a=1')
+        await self.kernel_client.execute("a=1")
+        assert self.kernel_manager.kernel is not None
         assert self.kernel_manager.kernel.shell.user_ns.get('a') == 1
